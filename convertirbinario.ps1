dba0d34d7fd60040ecd5c685885575c393de8c17
JaimeOrtegaS-C <91250023+JaimeOrtegaS-C@users.noreply.github.com>
2021-09-24 13:04:06 +0200

    Add files via upload

---
+++ convertirbinario.ps1
@@ -0,0 +1,23 @@
+﻿write-host " 1. pasar de binario a decimal"
+write-host " 2. pasar de hexadecimal a binario"
+$option = read-host "dime que que quieres convertir "
+
+if ($option -eq 1)
+{
+    $binario = read-host "dime el numero binario "
+    $decimal = [convert]::ToInt32($binario,2)
+    $decimal
+
+}
+
+if ($option -eq 2)
+{
+    $hexadecimal = read-host "dime el numero hexadecimal "
+    $decimal = [convert]::ToInt32($hexadecimal,16)
+    $binario = [convert]::ToString($decimal, 2)
+    $binario
+
+}
+
+
+ 
